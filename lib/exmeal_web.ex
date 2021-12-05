0390fcb9a7621f8b652fe7d8ea81a5c703715565
Francine Guimarães <francinesguimaraes@outlook.com>
2021-12-05 02:28:37 -0300

    chore: clean up project

--- lib/exmeal_web.ex
+++ lib/exmeal_web.ex
@@ -51,13 +51,6 @@ defmodule ExmealWeb do
     end
   end
 
-  def channel do
-    quote do
-      use Phoenix.Channel
-      import ExmealWeb.Gettext
-    end
-  end
-
   defp view_helpers do
     quote do
       # Import basic rendering functionality (render, render_layout, etc)
